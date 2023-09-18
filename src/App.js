1833e3174c0a73f7426f4958fe28d90c4979ba4c
AnhMinh <AnhMinh>
2023-09-19 00:55:14 +0700

    add style

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./css/style.css"
 function App() {
   return (
     <div className="calculator-grid">
@@ -6,6 +7,23 @@ function App() {
         <div className="current-operand"></div>
       </div>
       <button className="span-two">AC</button>
+      <button data-delete>DEL</button>
+      <button data-operation>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button data-operation>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button data-operation>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button data-operation>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button class="span-two">=</button>
     </div>
   );
 }
